916ab5e60d25cf835182380d522e79905d177fe6
Romulo Fritzen <romulofritzen22@gmail.com>
2026-01-17 13:43:32 -0300

    remove variável juros e a função round

--- calculadora_de_juros_compostos.py
+++ calculadora_de_juros_compostos.py
@@ -1,17 +1,13 @@
 #coleta dados do usuário
-valor_inicial = float(input("Valor inicial: "))
-taxa_mensal = float(input("Taxa de juros (mensal): "))
-meses = int(input("Tempo investido (meses): "))
+valor_inicial = float(input('Valor inicial: R$'))
+taxa_mensal = float(input('Taxa de juros (mensal): '))
+meses = int(input('Tempo investido (meses): '))
 
-#transforma a porcentagem em decimal
-taxa_mensal = taxa_mensal / 100
+#transforma a porcentagem em um número decimal 
+taxa_mensal = taxa_mensal / 100 + 1
 
 #fórmula dos juros compostos
-montante_final = valor_inicial * (1 + taxa_mensal) ** meses
-
-#calcula os juros
-juros = montante_final - valor_inicial
+montante_final = valor_inicial * taxa_mensal ** meses
 
 #imprime as informações ao usuário
-print("Ao final de", meses, "meses você terá um valor total de", round(montante_final, 2),", sendo desse valor", round(juros, 2), "o rendimento de juros.")
-
+print(f'Ao final de {meses} meses você terá um valor total de R${montante_final:.2f} sendo desse total R${montante_final - valor_inicial:.2f} o rendimento de juros.')
